d32834fd70a3e32e4d3d0308798ee351279d85c2
Tatiana Chock <chock.tatiana@outlook.com>
2025-06-27 15:13:10 -0600

    more unit tests

--- FoodForBots.Core/RecipeTheWrongWay.cs
+++ FoodForBots.Core/RecipeTheWrongWay.cs
@@ -1,36 +1,130 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 namespace FoodForBots.Core
 {
-    public class RecipeTheWrongWay
-    {
-        public string Name { get; set; } = "AnyOldTextInHere1";
-        public object Inputs { get; set; }
-        public object Output { get; set; }
-
-    }
     public class RecipeBetterWay
     {
         public string Name { get; set; } = "AnyOldTextInHere2";
-        public List<Ingredient> Ingredients { get; set; }
-        public Dish Dish { get; set; }
+        public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
+        public Dish Dish { get; set; } = new Dish();
     }
     public class Ingredient
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public IngredientType Type { get; set; }
+    }
+    public class FruitIngredient : Ingredient
+    {
+        public string Name { get; set; } = string.Empty;
+        public FruitEnum Type { get; set; }
+    }
+    public class Fruit
+    {
+        public virtual void Cut()
+        {
+
+        }
+    }
+    public enum FruitEnum
+    {
+        Apple = 2000,
+        Banana,
+        Watermelon,
+        Orange,
 
     }
+    public interface IFruit
+    {
+        void Cut();
+    }
+    public abstract class BaseFruit
+    {
+        public abstract void Cut();
+    
+    }
+    public abstract class IngredientType
+    {
+        public IngredientType(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; } = string.Empty;
+    }
+    public class AppleType : IngredientType
+    {
+        public AppleType() : base("Apple")
+        {
+
+        }
+    }
+
+    public class Apple : Ingredient, IFruit
+    {
+        public Apple()
+        {
+            Name = "Apple";
+        }
+
+        public void Cut()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Orange : Ingredient
+    {
+        public Orange()
+        {
+            Name = "Orange";
+        }
+    }
+    public class Watermelon : BaseFruit
+    {
+        public Watermelon()
+        {
+            
+        }
+
+        public override void Cut()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Banana : Fruit
+    {
+        public override void Cut()
+        {
+            
+           base.Cut();
+        }
+        public Banana()
+        {
+        
+        }
+    }
+    public class Grape : Ingredient
+    {
+        public Grape()
+        {
+            Name = "Grape";
+        }
+    }
+
     public class Dish
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
     }
     public class RecipeBestWay
     {
+        public RecipeBestWay(List<Ingredient> ingredients)
+        {
+            this.ingredients = ingredients;
+        }
         public string Name { get; set; } = "AnyOldTextInHere3";
         private readonly List<Ingredient> ingredients;
         public IEnumerable<Ingredient> Ingredients => ingredients;
-        public Dish Dish { get; set; }
+        public Dish Dish { get; set; } = new Dish();
     }
 }
--- FoodForBots.xTests/UnitTest1.cs
+++ FoodForBots.xTests/UnitTest1.cs
@@ -7,11 +7,155 @@ namespace FoodForBots.xTests
         [Fact]
         public void Test1()
         {
-            var worst = new RecipeTheWrongWay();
             var better = new RecipeBetterWay();
-            var best = new RecipeBestWay();
-            worst.Name = "AnyOldTextInHere2";
-            Assert.Equal("AnyOldTextInHere2", worst.Name);
+            var best = new RecipeBestWay(new List<Ingredient>());
+
+            better.Name = "AnyOldTextInHere1";
+            best.Name = "AnyOldTextInHere2";
+
+            Assert.Equal("AnyOldTextInHere1", better.Name);
+            Assert.Equal("AnyOldTextInHere2", best.Name);
+        }
+        [Fact]
+        public void Test2()
+        {
+            var ingredient = new Ingredient();
+            var ingredients = new List<Ingredient>() { ingredient };
+            var better = new RecipeBetterWay();
+            var best = new RecipeBestWay(ingredients);
+
+            better.Ingredients = ingredients;
+
+            Assert.Equal(ingredient, better.Ingredients.First());
+            Assert.Equal(ingredient, best.Ingredients.First());
+        }
+        [Fact]
+        public void Test3()
+        {
+            var dish = new Dish();
+            var better = new RecipeBetterWay();
+            var best = new RecipeBestWay(new List<Ingredient>());
+
+            better.Dish = dish;
+            best.Dish = dish;
+
+            Assert.Equal(dish, better.Dish);
+            Assert.Equal(dish, best.Dish);
+        }
+        [Fact]
+        public void Test4()
+        {
+            var something = new Ingredient();
+            var apple = new Apple();
+            var watermelon = new Watermelon();
+            var banana = new Banana();
+            var grape = new Grape();
+            var orange = new Orange();
+            var fruitSomething = new FruitIngredient();
+
+            something.Type = new AppleType();
+            fruitSomething.Type = FruitEnum.Apple;
+            fruitSomething.Name = "Apple";
+            switch (fruitSomething.Type)
+            {
+                case FruitEnum.Apple:
+                    Assert.True(true);
+                    break;
+                case FruitEnum.Banana:
+                    Assert.True(true);
+                    break;
+                case FruitEnum.Watermelon:
+                    Assert.True(true);
+                    break;
+            
+            }
+           if(something.Type is AppleType type)
+            {
+                Assert.Equal("Apple", type.Name);
+            }
+            Assert.Equal("Apple", apple.Name);
+        }
+        [Fact]
+        public void Test5()
+        {
+            var something = new Ingredient();
+            var apple = new Apple();
+            var watermelon = new Watermelon();
+            var banana = new Banana();
+            var grape = new Grape();
+            var orange = new Orange();
+            var fruitSomething = new FruitIngredient();
+
+            something.Type = new AppleType();
+            fruitSomething.Type = FruitEnum.Apple;
+            switch (fruitSomething.Type)
+            {
+                case FruitEnum.Apple:
+                    Assert.True(true);
+                    break;
+                case FruitEnum.Banana:
+                    Assert.True(true);
+                    break;
+                case FruitEnum.Watermelon:
+                    Assert.True(true);
+                    break;
+
+            }
+        }
+        [Fact]
+        public void Test6()
+        {
+            var something = new Ingredient();
+            var apple = new Apple();
+            var watermelon = new Watermelon();
+            var banana = new Banana();
+            var grape = new Grape();
+            var orange = new Orange();
+            var fruitSomething = new FruitIngredient();
+
+            something.Type = new AppleType();
+            fruitSomething.Type = FruitEnum.Apple;
+            switch (fruitSomething.Type)
+            {
+                case FruitEnum.Apple:
+                    Assert.True(true);
+                    break;
+                case FruitEnum.Banana:
+                    Assert.True(true);
+                    break;
+                case FruitEnum.Watermelon:
+                    Assert.True(true);
+                    break;
+
+            }
+        }
+        [Fact]
+        public void Test7()
+        {
+            var something = new Ingredient();
+            var apple = new Apple();
+            var watermelon = new Watermelon();
+            var banana = new Banana();
+            var grape = new Grape();
+            var orange = new Orange();
+            var fruitSomething = new FruitIngredient();
+
+            something.Type = new AppleType();
+            fruitSomething.Type = FruitEnum.Orange;
+            switch (fruitSomething.Type)
+            {
+                case FruitEnum.Apple:
+                    Assert.True(true);
+                    break;
+                case FruitEnum.Banana:
+                    Assert.True(true);
+                    break;
+                case FruitEnum.Watermelon:
+                    Assert.True(true);
+                    break;
+                default: Assert.Fail();
+                    break;
+            }
         }
     }
 }
